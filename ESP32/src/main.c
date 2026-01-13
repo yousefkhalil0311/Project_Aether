208de4f9325fd897bd62608c941a4367b2279f06
Yousef Khalil <yousefkhalil0311@gmail.com>
2026-01-13 12:07:04 -0500

    Addition to prj.conf: Enabled random-num-generator(git add .git add .not cryptographically securegit add .git add .).\n Added boards folder to ESP32 directory for specific revision .conf files for future hardware/firmware revisions.\n Updated main.c to demo RNG. Prints psuedo-random number every second.\n Use this command in the ESP32 directory to build project: west build -p always -b adafruit_feather_esp32s3/esp32s3/procpu -- -DEXTRA_CONF_FILE=boards/prj_Aether_Rev1_0.conf\n Use the following command to flash device: west flash

--- ESP32/src/main.c
+++ ESP32/src/main.c
@@ -1,6 +1,26 @@
+// Zephyr includes
+#include "zephyr/random/random.h"
+
+// Project includes
 #include "my_lib.h"
 
 int main() {
-    say_hello();
+
+    //Do forever
+    while(1){
+
+        //Generate and print psuedo-random number
+        uint32_t randNum = sys_rand32_get();
+
+        printk("Random Number: %u\n", randNum);
+
+        //Call hello function from my_lib
+        say_hello();
+
+        //Sleep for 1 second
+        k_msleep(1000);
+
+    }
+
     return 0;
 }
